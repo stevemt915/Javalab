593dc5c0286f78d547624f63e53f58d6fdda139c
stevemt915 <117297187+stevemt915@users.noreply.github.com>
2023-09-26 14:47:38 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,8 @@
+
+public class Sum {
+	public static void main(String args[]) {
+		int FirstNumber=31,SecondNumber=21,Sum;
+		Sum=FirstNumber+SecondNumber;
+		System.out.println("sum of the NOS are"+Sum);
+	}
+}
